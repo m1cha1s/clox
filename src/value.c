927ce19f3d9aa4a45408dee790a46e1eef1ddbe3
m1cha1s <msulej04@gmail.com>
2022-08-16 12:20:23 +0200

    Starting Strings

--- src/value.c
+++ src/value.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 #include "memory.h"
 #include "value.h"
@@ -33,5 +34,15 @@ void printValue(Value value) {
 			break;
 		case VAL_NIL: printf("nil"); break;
 		case VAL_NUMBER: printf("%g", AS_NUMBER(value)); break;
+	}
+}
+
+bool valuesEqual(Value a, Value b) {
+	if (a.type != b.type) return false;
+	switch (a.type) {
+		case VAL_BOOL:   return AS_BOOL(a) == AS_BOOL(b);
+		case VAL_NIL:    return true;
+		case VAL_NUMBER: return AS_NUMBER(a) == AS_NUMBER(b);
+		default:         return false;
 	}
 }

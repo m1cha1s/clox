877387d26f2927bd42ca8270d6702839d8efdefb
m1cha1s <msulej04@gmail.com>
2022-08-17 17:53:33 +0200

    Finnished While loops 23.4

--- src/compiler.c
+++ src/compiler.c
@@ -125,12 +125,39 @@ static void emitBytes(uint8_t byte1, uint8_t byte2) {
 	emitByte(byte2);
 }
 
+static void emitLoop(int loopStart) {
+	emitByte(OP_LOOP);
+	
+	int offset = currentChunk()->count - loopStart + 2;
+	if (offset > UINT16_MAX) error("Loop body too large.");
+	
+	emitByte((offset >> 8) & 0xff);
+	emitByte(offset & 0xff);
+}
+
+static int emitJump(uint8_t instruction) {
+	emitByte(instruction);
+	emitBytes(0xff, 0xff);
+	return currentChunk()->count - 2;
+}
+
 static void emitReturn() {
 	emitByte(OP_RETURN);
 }
 
 static void emitConstant(Value value) {
 	writeConstant(currentChunk(), value, parser.previous.line);
+}
+
+static void patchJump(int offset) {
+	int jump = currentChunk()->count - offset - 2;
+	
+	if (jump > UINT16_MAX) {
+		error("Too much code to jump over.");
+	}
+	
+	currentChunk()->code[offset] = (jump >> 8) & 0xff;
+	currentChunk()->code[offset + 1] = jump & 0xff;
 }
 
 static void initCompiler(Compiler* compiler) {
@@ -172,10 +199,22 @@ static void declaration();
 static uint32_t identifierConstant(Token* name);
 static int resolveLocal(Compiler* compiler, Token* name);
 static void markInitialized();
+static void and_(bool canAssign);
 
 static void grouping(bool canAssign) {
 	expression();
 	consume(TOKEN_RIGHT_PAREN, "Expect ')' after expression.");
+}
+
+static void or_(bool canAssign) {
+	int elseJump = emitJump(OP_JUMP_IF_FALSE);
+	int endJump = emitJump(OP_JUMP);
+	
+	patchJump(elseJump);
+	emitByte(OP_POP);
+	
+	parsePrecedence(PREC_OR);
+	patchJump(endJump);
 }
 
 static void number(bool canAssign) {
@@ -247,7 +286,7 @@ ParseRule rules[] = {
 	  [TOKEN_IDENTIFIER]    = {variable,     NULL,   PREC_NONE},
 	  [TOKEN_STRING]        = {string,     NULL,   PREC_NONE},
 	  [TOKEN_NUMBER]        = {number,   NULL,   PREC_NONE},
-	  [TOKEN_AND]           = {NULL,     NULL,   PREC_NONE},
+	  [TOKEN_AND]           = {NULL,     and_,   PREC_AND},
 	  [TOKEN_CLASS]         = {NULL,     NULL,   PREC_NONE},
 	  [TOKEN_ELSE]          = {NULL,     NULL,   PREC_NONE},
 	  [TOKEN_FALSE]         = {literal,     NULL,   PREC_NONE},
@@ -255,7 +294,7 @@ ParseRule rules[] = {
 	  [TOKEN_FUN]           = {NULL,     NULL,   PREC_NONE},
 	  [TOKEN_IF]            = {NULL,     NULL,   PREC_NONE},
 	  [TOKEN_NIL]           = {literal,     NULL,   PREC_NONE},
-	  [TOKEN_OR]            = {NULL,     NULL,   PREC_NONE},
+	  [TOKEN_OR]            = {NULL,     or_,   PREC_OR},
 	  [TOKEN_PRINT]         = {NULL,     NULL,   PREC_NONE},
 	  [TOKEN_RETURN]        = {NULL,     NULL,   PREC_NONE},
 	  [TOKEN_SUPER]         = {NULL,     NULL,   PREC_NONE},
@@ -326,6 +365,15 @@ static void defineVariable(uint32_t global) {
 	}
 
 	emitBytes(OP_DEFINE_GLOBAL, global);
+}
+
+static void and_(bool canAssign) {
+	int endJump = emitJump(OP_JUMP_IF_FALSE);
+	
+	emitByte(OP_POP);
+	parsePrecedence(PREC_AND);
+	
+	patchJump(endJump);
 }
 
 static uint32_t makeConstant(Value value) {
@@ -429,10 +477,43 @@ static void expressionStatement() {
 	emitByte(OP_POP);
 }
 
+static void ifStatement() {
+	consume(TOKEN_LEFT_PAREN, "Expected '(' after 'if'.");
+	expression();
+	consume(TOKEN_RIGHT_PAREN, "Expected ')' after condition.");
+	
+	int thenJump = emitJump(OP_JUMP_IF_FALSE);
+	emitByte(OP_POP);
+	statement();
+	
+	int elseJump = emitJump(OP_JUMP);
+	
+	patchJump(thenJump);
+	emitByte(OP_POP);
+	
+	if (match(TOKEN_ELSE)) statement();
+	patchJump(elseJump);
+}
+
 static void printStatement() {
 	expression();
 	consume(TOKEN_SEMICOLON, "Expected ';' after value.");
 	emitByte(OP_PRINT);
+}
+
+static void whileStatement() {
+	int loopStart = currentChunk()->count;
+	consume(TOKEN_LEFT_PAREN, "Expected '(' after 'while'.");
+	expression();
+	consume(TOKEN_RIGHT_PAREN, "Expected ')' after condition.");
+	
+	int exitJump = emitJump(OP_JUMP_IF_FALSE);
+	emitByte(OP_POP);
+	statement();
+	emitLoop(loopStart);
+	
+	patchJump(exitJump);
+	emitByte(OP_POP);
 }
 
 static void synchronize() {
@@ -472,6 +553,10 @@ static void declaration() {
 static void statement() {
 	if (match(TOKEN_PRINT)) {
 		printStatement();
+	} else if (match(TOKEN_IF)) {
+		ifStatement();
+	} else if (match(TOKEN_WHILE)) {
+		whileStatement();
 	} else if (match(TOKEN_LEFT_BRACE)) {
 		beginScope();
 		block();
